0558c7d8703e18a425d8c7f90a666f12ec08d72a
Tridiktya Putra <tridiktya@gmail.com>
2024-12-20 09:32:30 +0700

    feat: app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3010;
+
+app.get('/', (req, res, next) => {
+    return res.send("Hello with CI/CD!")
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`)
+})
